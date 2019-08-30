79fcf532a9b083396f77d892fcabfd1c84bd7a2c
Micah Zaayer <micahzaayer@gmail.com>
2019-08-30 09:56:10 -0700

    add non-working totalCost prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-
+// var userName
+// var sizes
+// var toppings
+// var total
 
 function Pizza(size, topping) {
   this.size = size,
@@ -6,17 +9,19 @@ function Pizza(size, topping) {
 }
 
 Pizza.prototype.totalCost = function() {
-
+  return sizes + toppings;
 }
 
 $(document).ready(function() {
-  var userName = $("input#userName").val();
-  var size = $("select#size").val();
-  var topping = $("select#topping").val();
-  var newPizza = new Pizza (size, topping)
   $("#pizza-order").submit(function(event) {
-
-
+    event.preventDefault();
+    var userName = parseInt($("input#userName").val());
+    var sizes = parseInt($("select#size").val());
+    var toppings = parseInt($("select#topping").val());
+    var newPizza = new Pizza (sizes, toppings);
+    var total = newPizza.totalCost();
+    console.log(newPizza);
+    console.log(total);
 
 
 
