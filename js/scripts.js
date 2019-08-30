d096bb848df00be1d5ed3accac28732f4d7936d7
Micah Zaayer <micahzaayer@gmail.com>
2019-08-30 11:07:42 -0700

    add sides and total cost output.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ var userName
 var sizes
 var toppings
 var total
+var salad
+var knots
+var cookie
+var brownine
 
 function Pizza(size, topping) {
   this.size = size,
@@ -9,26 +13,26 @@ function Pizza(size, topping) {
 }
 
 Pizza.prototype.totalCost = function() {
-  return sizes + toppings;
+  return sizes + toppings + salad;
 }
 
 $(document).ready(function() {
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
-    userName = $("input#userName").val();
+    userName = $("input#name").val();
     sizes = parseInt($("select#size").val());
     toppings = parseInt($("select#topping").val());
+    salad = $("#salad").val();
+
     var newPizza = new Pizza (sizes, toppings);
     total = newPizza.totalCost();
-    console.log(newPizza);
-    console.log(total);
-    console.log(userName);
-    $("#name").text(userName);
+    $(".output").show();
+    $("#nameOutput").text(userName);
+    $("#totalOutput").text(total);
 
 
 
-
-
+console.log(salad);
 
 
 
