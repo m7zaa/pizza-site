fbed75a8a31b83c57c260b6cd75acd5f2884e99c
Micah Zaayer <micahzaayer@gmail.com>
2019-09-04 19:12:39 -0700

    remove global variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
 //backend logic
 
-var userName
-var phone
-var sizes
-var toppings
-var total
-var salad
-var knots
-var cookie
-var brownie
-var sides
-
 //pizza constructer
 function Pizza(size, topping, side) {
   this.size = size,
@@ -19,24 +8,24 @@ function Pizza(size, topping, side) {
 }
 //prototype method to calculate total cost
 Pizza.prototype.totalCost = function() {
-  return sizes + toppings + sides;
+  return this.size + this.topping + this.side;
 }
 
 //ui
 $(document).ready(function() {
   $("#pizza-order").submit(function(event) {
     event.preventDefault();
-    userName = $("input#name").val();
-    phone = $("input#phone").val();
-    sizes = parseInt($("select#size").val());
-    toppings = parseInt($("select#topping").val());
-    salad = parseInt($("select#salad").val());
-    knots = parseInt($("select#garlicKnots").val());
-    cookie = parseInt($("select#cookie").val());
-    brownie = parseInt($("select#brownie").val());
-    sides = (knots + salad + cookie + brownie);
+    var userName = $("input#name").val();
+    var phone = $("input#phone").val();
+    var sizes = parseInt($("select#size").val());
+    var toppings = parseInt($("select#topping").val());
+    var salad = parseInt($("select#salad").val());
+    var knots = parseInt($("select#garlicKnots").val());
+    var cookie = parseInt($("select#cookie").val());
+    var brownie = parseInt($("select#brownie").val());
+    var sides = (knots + salad + cookie + brownie);
     var newPizza = new Pizza (sizes, toppings, sides);
-    total = newPizza.totalCost();
+    var total = newPizza.totalCost();
     if (total === 0 || newPizza.topping > newPizza.size) {
       $(".noToppings").hide();
       $(".zero").show();
